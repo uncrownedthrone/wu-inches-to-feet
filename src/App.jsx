8f1207ba81c47f3e0778dea25325c22e7696f59b
Justin Swiger <justin.swiger@me.com>
2019-12-04 09:54:31 -0500

    user input showing in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import InchesToFeet from './components/InchesToFeet'
 
 const App = () => {
--- src/components/InchesToFeet.jsx
+++ src/components/InchesToFeet.jsx
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+
+// DONE take user input and store it
+
+// TODO use stored user input and convert inches to feet
+// TODO display the results in the feet placeholder
 
 const InchesToFeet = () => {
+  const [input, setInput] = useState('')
+  console.log(input)
   return (
     <>
-      <input type="number" placeholder="inches" />
+      <input
+        type="number"
+        value={input}
+        onInput={e => setInput(e.target.value)}
+      />
       <button>Convert to Feet</button>
-      <p>Feet - [place]</p>
+      <p>Feet - [placeholder]</p>
     </>
   )
 }
