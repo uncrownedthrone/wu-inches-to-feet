5c36a3b14da866fe8ede51c2cc9c04373e8d39d9
Justin Swiger <justin.swiger@me.com>
2019-12-04 09:59:49 -0500

    results displayed on page

--- src/components/InchesToFeet.jsx
+++ src/components/InchesToFeet.jsx
@@ -1,22 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 // DONE take user input and store it
+// DONE use stored user input and convert inches to feet
+// DONE display the results in the feet placeholder
 
-// TODO use stored user input and convert inches to feet
-// TODO display the results in the feet placeholder
+// TODO convert on click
+// TODO convert decimals to feet and inches
 
 const InchesToFeet = () => {
   const [input, setInput] = useState('')
+  const converter = input / 12
   console.log(input)
+
+  useEffect(() => {
+    setInput()
+  }, [])
+
   return (
     <>
       <input
         type="number"
         value={input}
+        placeholder="enter inches"
         onInput={e => setInput(e.target.value)}
       />
-      <button>Convert to Feet</button>
-      <p>Feet - [placeholder]</p>
+      <p>{converter} feet</p>
     </>
   )
 }
